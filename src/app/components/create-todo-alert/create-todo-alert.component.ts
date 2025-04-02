0c8db681c815598cdcd69e9a64585af05eb99364
Fynn Berger <lachsfilet.coder@gmail.com>
2025-04-02 10:26:58 +0200

    added validation to todo creation

--- src/app/components/create-todo-alert/create-todo-alert.component.ts
+++ src/app/components/create-todo-alert/create-todo-alert.component.ts
@@ -1,14 +1,15 @@
 import {Component, Inject, OnDestroy} from '@angular/core';
 import {
-  MAT_DIALOG_DATA,
+  MAT_DIALOG_DATA, MatDialog,
   MatDialogActions,
   MatDialogClose,
   MatDialogContent,
   MatDialogTitle
 } from '@angular/material/dialog';
 import {TodoService} from '../../services/todo/todo.service';
-import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
 import {Subscription} from 'rxjs';
+import {AlertComponent, DialogType} from '../alert/alert.component';
 
 @Component({
   selector: 'app-create-tod-alert',
@@ -23,13 +24,12 @@ import {Subscription} from 'rxjs';
   styleUrl: './create-todo-alert.component.css'
 })
 
-// TODO: validation
 export class CreateTodoAlertComponent implements OnDestroy {
-  todo: FormControl<string> = new FormControl('')
+  todo: FormControl<string> = new FormControl('', Validators.required)
   employeeId: number
   subs = new Subscription()
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private todoService: TodoService) {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private todoService: TodoService, private dialog: MatDialog) {
     this.employeeId = data.employeeId
   }
 
@@ -38,12 +38,22 @@ export class CreateTodoAlertComponent implements OnDestroy {
   }
 
   createTodo() {
-    this.subs.add(
-      this.todoService.createTodo({
-        title: this.todo.value,
-        completed: false,
-        employeeId: this.employeeId
-      }).subscribe()
-    )
+      if(this.todo.invalid) {
+        this.dialog.open(AlertComponent, {
+          data: {
+            title: "Error",
+            message: "Please enter a Todo",
+            type: DialogType.ERROR
+          }
+        })
+      } else {
+        this.subs.add(
+          this.todoService.createTodo({
+            title: this.todo.value,
+            completed: false,
+            employeeId: this.employeeId
+          }).subscribe()
+        )
+      }
   }
 }
